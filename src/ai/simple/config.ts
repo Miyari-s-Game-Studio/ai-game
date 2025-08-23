4d85faadc39136a859ef2b9f595c76f2eee214e0
wooyme <867653608@qq.com>
2025-08-24 00:43:32 +0800

    reimplement AI flow. Make it simple

--- src/ai/simple/config.ts
+++ src/ai/simple/config.ts
@@ -1,1 +1,3 @@
-export const API_GENERATE = 'http://localhost:9601/llm/generate'
+import { config } from 'dotenv';
+config()
+export const API_GENERATE = process.env.API_GENERATE;//'http://localhost:9601/llm/generate'
